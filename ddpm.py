13e422e8708d95159686b610ca03e35424da0b40
Buffalo <8745050+buffalo12345@user.noreply.gitee.com>
2025-03-24 23:09:22 +0800

    ddpm codes

--- ddpm.py
+++ ddpm.py
@@ -1,3 +1,5 @@
 def linear_beta_schedule(timesteps):
-    scale - 1000/timesteps
-    
+    scale = 1000/timesteps
+    bate_start = scale * 0.0001
+    beta_end = scale * 0.02
+    return torch.linspace(beta_start, beta_end, timesteps)
